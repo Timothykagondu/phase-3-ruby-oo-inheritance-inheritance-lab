c14362244eb0b85939b828075c4ea8f2aab73135
TimothyKagondu <timothy.2k21@gmail.com>
2023-02-18 11:58:24 +0300

    completed assignment

---
+++ lib/user.rb
@@ -0,0 +1,13 @@
+class User
+    attr_reader :first_name, :last_name
+
+def first_name=(first_name)
+    @first_name = first_name
+        
+end
+    
+def last_name=(last_name)
+    @last_name = last_name
+    
+end
+end
